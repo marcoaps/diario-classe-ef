d278cb2d1baab628173cf5ddf03f49b8a7d154af
Marco Pedro <marcospedro1970@gmail.com>
2026-05-08 19:57:44 -0300

    fix: remover caractere extra do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-@'
-  import React, { useEffect, useState } from 'react';
+﻿  import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { StoreProvider } from './store';
 import { AppLayout } from './ui/AppLayout';
