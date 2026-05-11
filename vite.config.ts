07363b53b81e469bb90c1761f4e139eabead51b7
Marco Pedro <marcospedro1970@gmail.com>
2026-05-11 08:27:36 -0300

    fix base path vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/',
     assetsInclude: ['**/*.jpeg', '**/*.jpg', '**/*.png'],
     plugins: [react(), tailwindcss()],
     define: {
