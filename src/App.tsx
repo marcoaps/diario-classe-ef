557eb90d888a71938b8681d61e28e4d567eed46b
Marco Pedro <marcospedro1970@gmail.com>
2026-06-03 07:00:14 -0300

    feat: rota rendimento bimestre

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { StoreProvider } from './store';
 import { AppLayout } from './ui/AppLayout';
@@ -101,3 +101,4 @@ export default function App() {
     </BrowserRouter>
   );
 }
+
