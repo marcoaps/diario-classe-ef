87063bfc74d602ca228afd038eaacc30817d016d
Marco Pedro <marcospedro1970@gmail.com>
2026-06-13 13:27:35 -0300

    fix: remover numero da turma no cabecalho + remover pipes sublinhados

--- src/ui/pages/avaliacoes/AvaliacaoFolha.tsx
+++ src/ui/pages/avaliacoes/AvaliacaoFolha.tsx
@@ -90,8 +90,8 @@ function gerarHtmlProva(avaliacao: Avaliacao, aluno: Aluno): string {
           </td>
           <td style="padding:6px;vertical-align:middle;">
             <div style="font-size:11pt;font-weight:bold;margin-bottom:2px;">Avaliação - Ensino Fundamental - 2026</div>
-            <div style="font-size:10pt;margin-bottom:1px;">Disciplina: <strong>Educação Física</strong> &nbsp;|&nbsp; Professor(a): <strong>Marco Pedro</strong></div>
-            <div style="font-size:10pt;margin-bottom:1px;">Série: <strong>${serie}</strong> &nbsp;|&nbsp; Turma: <strong>${avaliacao.turma_id}</strong> &nbsp;|&nbsp; Nº: <strong>${aluno.numero_chamada}</strong></div>
+            <div style="font-size:10pt;margin-bottom:1px;">Disciplina: <strong>Educa&#231;&#227;o F&#237;sica</strong> &nbsp;&nbsp; Professor(a): <strong>Marco Pedro</strong></div>
+            <div style="font-size:10pt;margin-bottom:1px;">S&#233;rie: <strong>${serie}</strong> &nbsp;&nbsp; Turma: <strong>${avaliacao.turma_id.replace(/\d+/, '')}</strong> &nbsp;&nbsp; N&#186;: <strong>${aluno.numero_chamada}</strong></div>
             <div style="font-size:10pt;border-top:1px solid #cbd5e1;padding-top:3px;margin-top:3px;">Nome: <strong>${aluno.nome}</strong></div>
           </td>
         </tr>
